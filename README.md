3cfa264e197495e1be6e3fcf999734c30acd143b
ElvisAmonde <ogollanyanja@gmail.com>
2024-07-08 12:21:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+## Shopping list
+
+This project implements a basic shopping list webpage where users can add items, mark them as purchased, and clear the entire list. The application is built using HTML, CSS, and JavaScript for dynamic interaction with the user.
+
+## Features
+
+#### Add Items
+
+Users can enter items into an input field and click the "Add" button to append them to the shopping list.
+git
+
+#### Mark Purchased
+
+Items in the list can be marked as purchased by clicking on them. This action toggles a visual indicator (strikethrough) to signify the item has been acquired.
+
+Clear List: The "Clear List" button allows users to remove all items from the shopping list in one action.
+
+## Implementation Details
+
+HTML Structure: The webpage includes an input field for entering items, a list container and buttons for adding items, marking items as purchased, and clearing the list.
+
+\*JavaScript: DOM manipulation is handled through JavaScript to dynamically add new list items, toggle the purchased state of items, and clear the list.
+
+Array Management: JavaScript arrays are used to store and manage the list of shopping items. This array is manipulated based on user interactions (adding, marking, clearing).
+
+## Getting Started
+
+To run the shopping list webpage locally:
+
+Clone the reopsitory to your local machine
+
+bash
+Copy code
+git clone https://github.com/nyanja1/week2codechallange
+Open index.html: Open the index.html file in your web browser to view the webpage.
+
+## Interact with the Application:
+
+Use the input field to add new items,
+click on items to mark them as purchased,
+use the "Clear List" button to remove all items from the list.
+
+Example
+Here's a simple example of how the shopping list works:
+
+Initial State:
+
+Adding Items:
+
+Marking Items as Purchased:
+
+# Elvis
---
+++ index.js
@@ -0,0 +1,74 @@
+// // Creating the add button
+
+let addEl = document.createElement("button");
+addEl.textContent = "Add item";
+
+// Getting the input field and the text value
+let textEl = document.querySelector("#text-el");
+
+// Getting the main-container to append the button
+let mainContainer = document.querySelector("#main-container");
+mainContainer.appendChild(addEl);
+
+// Creating buttons "Mark as purchased" and "Clear list"
+let markPurcahesed = document.createElement("button");
+let clearList = document.createElement("button");
+
+// Adding text values for the buttons
+markPurcahesed.textContent = "Mark Purchased";
+clearList.textContent = "Clear List";
+
+// Appending the buttons
+let itemsEl = document.querySelector("#items-el");
+itemsEl.appendChild(markPurcahesed);
+itemsEl.appendChild(clearList);
+
+// Holding the list
+let arrayList = [];
+
+//  Adding an event listener for the add btn
+addEl.addEventListener("click", () => {
+  createElement(textEl.value);
+  textEl.value = "";
+});
+
+// Function to create the list
+function createElement(value) {
+  let li = document.createElement("li");
+  if (value === "") {
+    alert("Enter a valid item");
+  } else {
+    li.textContent = value;
+    let ul = document.querySelector("#list");
+    ul.appendChild(li);
+
+    // Adding items to the array
+
+    arrayList.push(value);
+    console.log(arrayList);
+
+    // Appending the li
+    purchasedItems(li);
+    deleteItems(li);
+  }
+}
+
+// Function for change color for the purchased items
+function purchasedItems(value) {
+  markPurcahesed.addEventListener("click", () => {
+    value.style.textDecoration = "line-through";
+    value.style.color = "green";
+  });
+}
+
+// Functions that delete all the list items
+function deleteItems(value) {
+  clearList.addEventListener("click", () => {
+    value.remove();
+    let index = arrayList.indexOf(value.textContent);
+    if (index !== -1) {
+      arrayList.splice(index, 1);
+      console.log(arrayList);
+    }
+  });
+}
